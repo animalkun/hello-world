0c98135d2a8041cf2c873c446286b5a707a4d8a1
animalkun <48173613+animalkun@users.noreply.github.com>
2019-10-27 17:30:07 +0800

    update the dorm work
    finished the backward propagation

--- 3_2_planar_data_classification.py
+++ 3_2_planar_data_classification.py
@@ -115,3 +115,16 @@ def backward_propagation(parameters, cache, X, Y):
     A2 = cache["A2"]
 
     # Backward propagation: calculate dW1, db1, dW2, db2.
+    dZ2 = A2 - Y
+    dW2 = np.dot(dZ2, A1.T)/m
+    db2 = np.sum(dZ2, axis=1, keepdims=True)/m
+    dZ1 = np.multiply(np.dot(W2.T, dZ2), (1-A1**2))
+    dW1 = np.dot(dZ1, X.T)/m
+    db1 = np.sum(dZ1, axis=1, keepdims=True)/m
+
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+
+    return grads
